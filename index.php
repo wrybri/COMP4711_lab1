0ce209daf5f7901b1fb5527f0bf05e3f2d25a81b
Brian Livesey <bri@brianlivesey.com>
2016-01-07 16:50:08 -0800

    First netbeans commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hello COMP4711</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello world! ...and COMP4711 BCIT students";
+        ?>
+    </body>
+</html>
+
